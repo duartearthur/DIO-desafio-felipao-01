086ae12ccf42b02ee039d2451a3508cc73c0f6e3
duartearthur <duarte.arthur@outlook.com>
2024-02-17 20:19:46 -0300

    tentativa de código

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
-let nome = "felipão"
-console.log(nome)
+let NomeHeroi = ("Arthur")
+let XpHeroi = (1200)
+
+if (XpHeroi < 1000){
+    console.log("Ferro")
+}
+else if (XpHeroi >= 1001 && XpHeroi <= 2000){
+    console.log("Bronze")
+}
+else if (XpHeroi >= 2001 && XpHeroi <= 5000){
+    console.log("Prata")
+}
+else if (XpHeroi >= 5001 && XpHeroi <= 7000){
+    console.log("Ouro")
+}
+else if (XpHeroi >= 7001 && XpHeroi <= 8000){
+    console.log("Platina")
+}
+else if (XpHeroi >= 8001 && XpHeroi <= 9000){
+    console.log("Ascendente")
+}
+else if (XpHeroi >= 7001 && XpHeroi <= 8000){
+    console.log("Platina")
+}
+else if (XpHeroi >= 9001 && XpHeroi <= 10000){
+    console.log("Imortal")
+}
+else if (XpHeroi >= 10001){
+    console.log("Radiante")
+}
+
+console.log ("O Herói de nome "+ NomeHeroi + "está no nível de "+ XpHeroi)
+
