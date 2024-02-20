340e919f42cc32dc3110a614a02b42f1c13ec787
duartearthur <duarte.arthur@outlook.com>
2024-02-19 22:36:02 -0300

    entrega do projeto

--- src/index.js
+++ src/index.js
@@ -1,33 +1,37 @@
+// Desafio do Felipão - DIO
+
 let NomeHeroi = ("Arthur")
-let XpHeroi = (1200)
+let XpHeroi = (50000)
+let XpNome = ("")
 
 if (XpHeroi < 1000){
-    console.log("Ferro")
+    let XpNome = ("Ferro")
 }
 else if (XpHeroi >= 1001 && XpHeroi <= 2000){
-    console.log("Bronze")
+        XpNome = ("Bronze")
 }
 else if (XpHeroi >= 2001 && XpHeroi <= 5000){
-    console.log("Prata")
+        XpNome = ("Prata")
 }
 else if (XpHeroi >= 5001 && XpHeroi <= 7000){
-    console.log("Ouro")
+        XpNome = ("Ouro")
 }
 else if (XpHeroi >= 7001 && XpHeroi <= 8000){
-    console.log("Platina")
+        XpNome = ("Platina")
 }
 else if (XpHeroi >= 8001 && XpHeroi <= 9000){
-    console.log("Ascendente")
+        XpNome = ("Ascendente")
 }
 else if (XpHeroi >= 7001 && XpHeroi <= 8000){
-    console.log("Platina")
+        XpNome = ("Platina")
 }
 else if (XpHeroi >= 9001 && XpHeroi <= 10000){
-    console.log("Imortal")
+        XpNome = ("Imortal")
 }
 else if (XpHeroi >= 10001){
-    console.log("Radiante")
+        XpNome = ("Radiante")
 }
 
-console.log ("O Herói de nome "+ NomeHeroi + "está no nível de "+ XpHeroi)
+console.log  ("O Herói de nome "+ NomeHeroi + " está no nível de "+ XpNome + " (XP="+ XpHeroi +")")
 
+// Só fiquei na dúvida onde e como eu poderia usar um laço de repetição no desafio, pensando exatamente no que foi pedido... git
